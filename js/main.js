6c01dfead2d1810c5af6b03c0272e9a60b0f468e
rana assy <accrana20@gmail.com>
2019-01-06 02:03:40 +0200

    make some edits

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ myApp.controller("myController", function($scope) {
     $scope.newUser = {};
 
     $scope.users = [
-        { name: "rana", age: "24", gender: "female" },
-        { name: "mohamed", age: "34", gender: "male" },
-        { name: "sherien", age: "30", gender: "female" },
-        { name: "wael", age: "20", gender: "male" }
+        { name: "rana", age: "24", gender: "female", photo: "images (1).png" },
+        { name: "mohamed", age: "34", gender: "male", photo: "images (2).png" },
+        { name: "sherien", age: "30", gender: "female", photo: "images (1).png" },
+        { name: "ahmed", age: "20", gender: "male", photo: "images (2).png" }
     ];
 
     $scope.selectUser = function(user) {
